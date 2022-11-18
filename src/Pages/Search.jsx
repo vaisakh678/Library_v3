32567ec089cd4cf257bc837eea4ede2483ac5c7b
vaisakh b <vaisakhb@vaisakhs-MacBook-Air.local>
2022-11-18 08:10:51 +0530

    initial commit🚀

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -1,23 +1,60 @@
-import React from "react";
-import { Stack, TextField } from "@mui/material";
+import React, { useState } from "react";
+import {
+    Stack,
+    TextField,
+    FormControl,
+    InputLabel,
+    Select,
+    MenuItem,
+    Button,
+} from "@mui/material";
 
 function Search() {
+    const [type, setType] = useState("student");
     return (
         <div className="h-full contentWidth">
             <div className="contentTitle bg-slate-50fuck border-b-2 border-slate-200 px-4 flex items-center">
                 Search
             </div>
-            <div className="contentContainer flex justify-center items-center bg-slate-200">
-                <Stack>
-                    <TextField
-                        id="standard-basic"
-                        label="Year"
-                        variant="standard"
-                        size="small"
-                        type="number"
-                        sx={{ width: 80 }}
-                    />
-                </Stack>
+            <div className="contentContainer flex justify-center p-4 items-center ">
+                <div className="content-wrapper h-full w-full bg-slate-200">
+                    <Stack direction="row" spacing={2}>
+                        <FormControl>
+                            <InputLabel id="demo-simple-select-label">
+                                Department
+                            </InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                sx={{ width: 160 }}
+                                id="demo-simple-select"
+                                value={type}
+                                label="Student Department"
+                                size="small"
+                                onChange={(e) => setType(e.target.value)}
+                            >
+                                <MenuItem value="mca">MCA</MenuItem>
+                                <MenuItem value="int-mca">Int-MCA</MenuItem>
+                                <MenuItem value="bfa">BFA</MenuItem>
+                            </Select>
+                        </FormControl>
+
+                        <TextField
+                            id="standard-basic"
+                            label="Search"
+                            variant="outlined"
+                            size="small"
+                            sx={{ width: 150 }}
+                        />
+
+                        <Button
+                            variant="contained"
+                            type="submit"
+                            sx={{ width: 100 }}
+                        >
+                            Submit
+                        </Button>
+                    </Stack>
+                </div>
             </div>
         </div>
     );
